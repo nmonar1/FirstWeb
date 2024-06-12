d310055161b86730ccfbf65dd10ae512c83dbe9d
Nicole Monari <nicole.monari@strathmore.edu>
2024-06-12 22:13:05 +0300

    used include function for the heads section in the products pages

--- products.php
+++ products.php
@@ -1,11 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Products</title>
-    <link rel="stylesheet" href="css/products.css">
-</head>
+<?php include_once("templates/header.php");?>
 <body>
 <?php include_once("templates/nav.php");?>
 <div class="container">
--- skincare.php
+++ skincare.php
@@ -1,13 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Skin care</title>
-    <link rel="stylesheet" href="css/products.css">
-    <?php include_once("templates/nav.php");?>
-   
-</head>
+<?php include_once("templates/header.php");?>
+<?php include_once("templates/nav.php");?>
 <body>
     <div class="container">
         <div class="row image-row">
