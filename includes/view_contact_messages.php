02a8f1afe31da88f67980b0e546186a52c296e4f
Nicole Monari <nicole.monari@strathmore.edu>
2024-07-02 14:59:09 +0300

    created view_contact_messages.php

---
+++ includes/view_contact_messages.php
@@ -0,0 +1,78 @@
+<?php
+require_once("db_connect.php");
+include_once("../templates/header.php");
+include_once("../templates/nav.php");
+
+if (isset($_GET["DelId"])) {
+    $DelId = mysqli_real_escape_string($conn, $_GET["DelId"]);
+    
+    // SQL to delete a record
+    $del_msg = "DELETE FROM `contact_us` WHERE messageId='$DelId' LIMIT 1";
+    
+    if ($conn->query($del_msg) === TRUE) {
+        header("Location: view_contact_messages.php");
+        exit();
+    } else {
+        echo "Error deleting record: " . $conn->error;
+    }
+}
+?>
+ 
+<div class="header">
+    <h1>Contact Form Messages</h1>
+</div>
+        
+<div class="row">
+    <div class="content">
+        <h1>Messages</h1>
+        <p>Below are the messages submitted through the contact form.</p>
+        <table>
+            <thead>
+                <tr>
+                    <th>SN</th>
+                    <th>Full Name</th>
+                    <th>Email</th>
+                    <th>Message</th>
+                    <th>Time</th>
+                    <th>Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+<?php
+$select_msg = "SELECT * FROM `contact_us` ORDER BY dateSubmitted DESC";
+$sel_msg_res = $conn->query($select_msg);
+$cm = 0;
+if ($sel_msg_res->num_rows > 0) {
+    while ($sel_msg_row = $sel_msg_res->fetch_assoc()) {
+        $cm++;
+?>
+                <tr>
+                    <td><?php echo $cm; ?>.</td>
+                    <td><?php echo htmlspecialchars($sel_msg_row["fullname"]); ?></td>
+                    <td><?php echo htmlspecialchars($sel_msg_row["email"]); ?></td>
+                    <td><?php echo '<strong>' . substr(htmlspecialchars($sel_msg_row["message"]), 0, 25) . '...</strong>'; ?></td>
+                    <td><?php echo date("d-M-Y H:i", strtotime($sel_msg_row["dateSubmitted"])); ?></td>
+                    <td>[ <a href="?DelId=<?php echo $sel_msg_row["messageId"]; ?>" onclick="return confirm('Are you sure you want to delete this message permanently from the database?')">Del</a> ]</td>
+                </tr>
+<?php
+    }
+} else {
+    echo "<tr><td colspan='6'>No results found</td></tr>";
+}
+?>
+            </tbody>
+            <thead>
+                <tr>
+                    <th>SN</th>
+                    <th>Full Name</th>
+                    <th>Email</th>
+                    <th>Message</th>
+                    <th>Time</th>
+                    <th>Actions</th>
+                </tr>
+            </thead>
+        </table>
+    </div>
+    
+</div>      
+<?php include_once("../templates/footer.php"); ?>
