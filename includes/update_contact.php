a20f9582d7437958c2bf7a4b47a627a737ec5d01
Nicole Monari <nicole.monari@strathmore.edu>
2024-07-08 08:57:09 +0300

    added comments

--- includes/update_contact.php
+++ includes/update_contact.php
@@ -1,30 +1,46 @@
 <?php
+// Include the database connection file
 require_once("db_connect.php");
+
+// Include the header and navigation templates
 include_once("../templates/header.php");
 include_once("../templates/nav.php");
 
+// Sanitize the messageId received from the GET request to prevent SQL injection
 $messageId = mysqli_real_escape_string($conn, $_GET["messageId"]);
 
+// Fetch the message from the contact_us table based on the messageId
 $spot_msg = "SELECT * FROM `contact_us` WHERE messageId = '$messageId' LIMIT 1";
 $spot_msg_res = $conn->query($spot_msg);
 $spot_msg_row = $spot_msg_res->fetch_assoc();
 
+// Check if the form to update the message has been submitted
 if (isset($_POST["update_message"])) {
+    // Sanitize the input fields received from the POST request to prevent SQL injection
+    //mysqli_real_escape_string is used to escape all special characters for use in an SQL query.
     $fullname = mysqli_real_escape_string($conn, $_POST["fullname"]);
     $email = mysqli_real_escape_string($conn, $_POST["email"]);
     $message = mysqli_real_escape_string($conn, $_POST["message"]);
     $messageId = mysqli_real_escape_string($conn, $_POST["messageId"]);
+    
 
+
+    // Update the message in the contact_us table based on the messageId
     $update_message = "UPDATE contact_us SET fullname = '$fullname', email = '$email', message = '$message' WHERE messageId='$messageId' LIMIT 1";
-  
+
+    // Check if the update query was successful and redirect to the view_contact_messages page
     if ($conn->query($update_message) === TRUE) {
+        //Redirects the user to the view_contact_messages.php page if the update was successful.
         header("Location: view_contact_messages.php");
         exit();
+
     } else {
+        // Display an error message if the update query failed
         echo "Error: " . $update_message . "<br>" . $conn->error;
     }
 }
 ?>
+
 
 <link rel="stylesheet" href="../css/contact_us.css">
 <div class="header">
